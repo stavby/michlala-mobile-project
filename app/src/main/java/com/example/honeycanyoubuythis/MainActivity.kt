22623ef26f4c20a54cdbaf2804f935fa2db1e136
stavby <stav134679@gmail.com>
2025-03-19 19:59:35 +0200

    Fix: scrapped activity login and setback everything to work with activity main

--- app/src/main/java/com/example/honeycanyoubuythis/MainActivity.kt
+++ app/src/main/java/com/example/honeycanyoubuythis/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.honeycanyoubuythis
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
