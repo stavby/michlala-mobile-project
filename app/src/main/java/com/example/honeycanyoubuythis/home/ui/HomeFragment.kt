3565c1a4a377078755efe952715342a06d8c5951
stavby <stav134679@gmail.com>
2025-03-20 10:38:29 +0200

    Feat: created a home page class and layout boilerplate

---
+++ app/src/main/java/com/example/honeycanyoubuythis/home/ui/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.honeycanyoubuythis.home.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import com.example.honeycanyoubuythis.databinding.LoginFragmentBinding
+
+class HomeFragment: Fragment() {
+    private var _binding: LoginFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
